1bee7c6b1a33d08052f92a164fdc385ecabee3da
John Petersen <sobeita@gmail.com>
2017-02-02 20:39:12 -0500

    Migrated minimize action from urgent.sh to exit-fullscreen.sh

--- bin/exit-fullscreen.sh
+++ bin/exit-fullscreen.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-xdotool key alt+F11
+res_pattern='\([0-9]*x[0-9]*\)'
+res=$(xdpyinfo | sed -n 's/[ ]*dimensions:[ ]*'"$res_pattern"'.*/\1/p')
+xdotool search --onlyvisible . 2>/dev/null | { \
+	while read i; do \
+		xdotool getwindowgeometry $i | \
+		sed -n 's/Window \([0-9]*\)\|Geometry: '"$res_pattern"'/\1\2/p' | {
+			read j && read k && [ "$k" = "$res" ] && \
+				xdotool windowminimize $i;
+	}; done; }
