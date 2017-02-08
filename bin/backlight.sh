4d9a76aea9fa499040d9c8f27971a911044c3778
John Petersen <sobeita@gmail.com>
2017-02-08 12:07:51 -0500

    Fixed assignment for directly setting brightness percentage ('let' follows Bash rules, so floating point values won't work)

--- bin/backlight.sh
+++ bin/backlight.sh
@@ -7,35 +7,27 @@ fi
 cur=$(cat /sys/class/backlight/intel_backlight/brightness)
 max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 cur_pc=$(echo "scale=2; $cur * 100 / $max" | bc)
-# cur_pc=$(($cur * 100 / $max))
-# cur_rem=$(($(($cur * 100)) % $max / 100))
 
 case "$1" in
 	'+' )
-		if [ -z "$2" ]; then
-			set -- "$1" 2
-		fi
+		[ -z "$2" ] && set -- "$1" 2
 		new_pc=$(echo "scale=2; $cur_pc + $2" | bc)
-		# new_pc=$(($cur_pc + $2));
 		;;
 	'-' )
-		if [ -z "$2" ]; then
-			set -- "$1" 1
-		fi
+		[ -z "$2" ] && set -- "$1" 1
 		new_pc=$(echo "scale=2; $cur_pc - $2" | bc)
-		# new_pc=$(($cur_pc - $2));
 		;;
 esac
 
 if [ -z "$new_pc" ]; then
-	let "new_pc=$1"
+	new_pc=$(echo $1)
 	# new_pc="$2"
 fi
 
 if [ $(echo "scale=2; $new_pc < 0" | bc) -eq 1 ]; then
-	new_pc=0
+	new_pc="0"
 elif [ $(echo "scale=2; $new_pc > 100" | bc) -eq 1 ]; then
-	new_pc=100
+	new_pc="100"
 fi
 
 new_val=$(echo "scale=0; $new_pc * $max / 100" | bc)
