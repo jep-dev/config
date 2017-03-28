0c2013ca1976f3ca1e3ed145453634c4c6c99216
John Petersen <sobeita@gmail.com>
2017-03-28 16:44:53 -0400

    Changed time formatting

--- bin/time.sh
+++ bin/time.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-date +\ %I%n:%M%n:%S%n\ %p%n%n%a%n%b%n\ %d
+#date +\ %I%n:%M%n:%S%n\ %p%n%n%a%n%b%n\ %d
+#date +\ %k%n:%M%n%n%b%n\ %d
+#date +\ %k:%_M%n\ \ \ %a%n\ %_m/%e
+date +---%n\ %k%n:%_M%n:%_S%n---%n%a%n%b%n\ %e%n---
