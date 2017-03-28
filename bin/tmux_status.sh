680095e7ff47bfd39a217d5c7b57adf40dd1b024
John Petersen <sobeita@gmail.com>
2017-03-28 16:45:14 -0400

    Added colors to Powerline-style prompt

--- bin/tmux_status.sh
+++ bin/tmux_status.sh
@@ -4,26 +4,38 @@ bold='#[bold]'
 def='#[default]'
 none='#[none]'
 inv='#[reverse]'
+bk='#[fg=0]'
 
 # Config
-# vert=$'\u26fe'' ' # user/host separator
-vert=$'\u26fe'' ' # user/host separator
+vert=' @ '
+#vert=$'\u26fe'' ' # user/host separator
 lbra=$'\ue0b2' # left bracket
 rbra=$'\ue0b0' # right bracket
-# cb=(252 255 240 236) # backgrounds
-#cb=(53 88 53 18 22)
-cb=(53 52 52 53 18 22)
-ci=( 1  2  1  0  0 -1) # bg index jump table
+
+#cb=(24 28 24 55 160 166 100 28) # backgrounds
+#cf=(15 15 15 15  15  15  15 15)
+# ci=( 1  0   1  0  0   0   0  -4) # bg index direction
+
+#cb=( 95 203  95 101 247)
+#cb=( 95 131 95  65 143 143)
+#cf=( 16  16 16  16  16  16)
+#ci=(  1   0  1   0   1  -1)
 sw=2 # index of center segment
 
-cf=($(for i (${cb[@]}) echo $((i>245?232:255))))
-bk='#[fg=0]'
+#cf=($(for i (${cb[@]}) echo $((i>245?232:255))))
+#cf=('colour'${^cf})
+#cb=('colour'${^cb})
+
+cb=('#993380' '#993333' '#993380' '#663399' '#334d99' '#339999' \
+	'#33994d' '#338899' '#33995e' '#559933' '#999133' '#993333')
+cf=(16 16 16 16 16 16 16 16 16 16 16 16)
+ci=( 1  0  1  0  0  0  0  0  0  0  0 -9)
 cf=('colour'${^cf})
-cb=('colour'${^cb})
+
 cn=${#cb}
 
 {
-	printf '%s\n%s %s %s\n' "$(battery.sh -g)" "$USER" "$vert" "$HOST"
+	printf '%s\n%s%s%s\n' "$(battery.sh -g)" "$USER" "$vert" "$HOST"
 	ps ho args t $(tmux display -p -F '#{pane_tty}')
 } | {
 	j=1
