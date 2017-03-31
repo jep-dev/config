70ac997f9fbda89b784ca5942cf4633aad010cc3
John Petersen <sobeita@gmail.com>
2017-03-30 20:11:44 -0400

    Added lhs/rhs status lines

--- bin/tmux_status.sh
+++ bin/tmux_status.sh
@@ -8,50 +8,32 @@ inv='#[reverse]'
 bk='#[fg=0]'
 
 # Config
-vert=' '$'\u273b'' ' # host/user separator
+vert=' '$'\u2731'' ' # host/user separator
 lbra=$'\ue0b2' # lhs segment separator
 rbra=$'\ue0b0' # rhs segment separator
 sw=2 # index where lhs/rhs segment separator switches
 
-cb=(
-'#320f56'
-'#0f2156'
-'#320f56'
-'#560f0f'
-'#560f0f'
-'#56440f'
-'#33560f'
-'#0f5621'
-'#0f5656'
-'#0f2156'
+join_segments(){
+	cb=('#320f56' '#0f2156' '#320f56' '#560f0f'
+		'#56440f' '#0f5621' '#0f5656' '#0f2156')
 
-#	'#f7d46d'
-#	'#f76d6d'
-#	'#f7d46d'
-#	'#b2f76d'
-#	'#6df790'
-#	'#6df7f7'
-#	'#6d90f7'
-#	'#b26df7'
-#	'#f76dd4'
-#	'#f76d6d'
+	# '#f7d46d' '#f76d6d' '#f7d46d' '#b2f76d' '#6df790'
+	# '#6df7f7' '#6d90f7' '#b26df7' '#f76dd4' '#f76d6d'
 
-# dark to light
-#'colour208' 'colour202' 'colour208' 'colour214' 'colour220' 'colour221' 'colour222'
-# light to dark
-#'colour220' 'colour222' 'colour220' 'colour214' 'colour208' 'colour202' 'colour208'
-)
-cf=(15 15 15 15 15 15 15 15 15)
-ci=( 1  0  1  0  0  0  0  0 -6)
-#cb=('colour'${^cb})
-cf=('colour'${^cf})
+	# dark to light
+	#  'colour208' 'colour202' 'colour208'
+	#  'colour214' 'colour220' 'colour221' 'colour222'
+	# light to dark
+	#  'colour220' 'colour222' 'colour220'
+	#  'colour214' 'colour208' 'colour202' 'colour208'
 
-cn=${#cb}
+	cf=(15 15 15 15 15 15 15 15)
+	ci=( 1  0  1  0  0  0  0 -5)
+	#cb=('colour'${^cb})
+	cf=('colour'${^cf})
 
-{
-	printf '%s\n' "$(battery.sh -g)" "$HOST$vert$USER"
-	ps ho args t $(tmux display -p -F '#{pane_tty}')
-} | {
+	cn=${#cb}
+
 	j=1
 	while read i; do
 		fmb='#[bg='${cb[j]}']'; fmab=${fmb//bg/fg}
@@ -65,3 +47,14 @@ cn=${#cb}
 		#j=$((j+ci[j]))
 	done
 }
+
+side=${1:-'-l'}
+if [ "$side" = "-l" ]; then
+	{
+		printf '%s\n' "$(battery.sh -g)" "$HOST$vert$USER"
+		ps ho args t $(tmux display -p -F '#{pane_tty}')
+		#echo -n {a..z}'\n'
+	} | join_segments
+elif [ "$side" = '-r' ]; then
+	time.sh 'wide' | join_segments
+fi
