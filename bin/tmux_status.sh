df4a3e234fc5b53c4db920e88daf617cd4077564
John Petersen <sobeita@gmail.com>
2017-03-31 13:48:32 -0400

    Tweaked status-right formatting

--- bin/tmux_status.sh
+++ bin/tmux_status.sh
@@ -11,23 +11,20 @@ bk='#[fg=0]'
 vert=' '$'\u2731'' ' # host/user separator
 lbra=$'\ue0b2' # lhs segment separator
 rbra=$'\ue0b0' # rhs segment separator
-sw=2 # index where lhs/rhs segment separator switches
 
 join_segments(){
-	cb=('#bf9f3f' '#bf3f3f'
-		'#bf9f3f' '#7fbf3f'
-		'#3fbf5f' '#3fbfbf'
-		'#3f5fbf' '#7f3fbf'
-		'#bf3f9f' '#bf3f3f')
-	cf=(16 16 16 16 16 16 16 16 16 16)
-	ci=( 1  0  1  0  0  0  0  0  0 -7)
+	sw=${1:-2}
+	cb=('#ffb265' '#ff6565' '#ffb265' '#feff65' '#b2ff65' '#66ff65' '#65ffb2'
+		'#65feff' '#65b2ff' '#6566ff' '#b265ff' '#ff65fe' '#ff65b2' '#ff6565')
+	ci=(1 0 1 0 0 0 0 0 0 0 0 0 0 -11)
 	cf=('colour'${^cf})
 	cn=${#cb}
 
 	j=1
 	while read i; do
 		fmb='#[bg='${cb[j]}']'; fmab=${fmb//bg/fg}
-		fmf='#[fg='${cf[j]}']'; fmaf=${fmf//fg/bg}
+		#fmf='#[fg='${cf[j]}']'; fmaf=${fmf//fg/bg}
+		fmf='#[fg=colour232]'; fmaf=${fmf//fg/bg}
 		[ $j -ge $sw ] && post=$fmab$rbra && pre=$inv$bk$fmab$rbra$def
 		[ $j -le $sw ] && pre=$bk$fmab$lbra
 		[ $j -lt $sw ] && post=$inv$bk$fmab$lbra$def
@@ -42,8 +39,8 @@ if [ "$side" = "-l" ]; then
 	{
 		printf '%s\n' "$(battery.sh -g)" "$HOST$vert$USER"
 		ps ho args t $(tmux display -p -F '#{pane_tty}')
-		#echo -n {a..q}'\n'
+		#echo -n {a..x}'\n'
 	} | join_segments
 elif [ "$side" = '-r' ]; then
-	{ echo && time.sh 'wide' } | join_segments
+	time.sh 'wide' | join_segments 1
 fi
