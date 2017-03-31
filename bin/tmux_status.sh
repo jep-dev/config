5d7ab6680a27aa71be48d279a4d087567a4fa97b
John Petersen <sobeita@gmail.com>
2017-03-31 12:18:16 -0400

    Changed color palette back to pastel

--- bin/tmux_status.sh
+++ bin/tmux_status.sh
@@ -14,10 +14,13 @@ rbra=$'\ue0b0' # rhs segment separator
 sw=2 # index where lhs/rhs segment separator switches
 
 join_segments(){
-	cb=('#320f56' '#0f2156' '#320f56' '#560f0f'
-		'#56440f' '#0f5621' '#0f5656' '#0f2156')
-	cf=(15 15 15 15 15 15 15 15)
-	ci=( 1  0  1  0  0  0  0 -5)
+	cb=('#bf9f3f' '#bf3f3f'
+		'#bf9f3f' '#7fbf3f'
+		'#3fbf5f' '#3fbfbf'
+		'#3f5fbf' '#7f3fbf'
+		'#bf3f9f' '#bf3f3f')
+	cf=(16 16 16 16 16 16 16 16 16 16)
+	ci=( 1  0  1  0  0  0  0  0  0 -7)
 	cf=('colour'${^cf})
 	cn=${#cb}
 
@@ -31,7 +34,6 @@ join_segments(){
 		printf '%s' $pre$fmb$fmf' '$i' '$def$post$def
 		[ ${ci[j]} -ne 0 ] && dj=${ci[j]}
 		j=$((j+dj))
-		#j=$((j+ci[j]))
 	done
 }
 
@@ -40,7 +42,7 @@ if [ "$side" = "-l" ]; then
 	{
 		printf '%s\n' "$(battery.sh -g)" "$HOST$vert$USER"
 		ps ho args t $(tmux display -p -F '#{pane_tty}')
-		#echo -n {a..z}'\n'
+		#echo -n {a..q}'\n'
 	} | join_segments
 elif [ "$side" = '-r' ]; then
 	{ echo && time.sh 'wide' } | join_segments
