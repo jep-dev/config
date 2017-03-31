ad3fc51d002606cf5176693ebf4c2d3c00461105
John Petersen <sobeita@gmail.com>
2017-03-31 12:18:26 -0400

    Tweaked time format

--- bin/time.sh
+++ bin/time.sh
@@ -2,7 +2,8 @@
 
 mode=${1:-'thin'}
 if [ "$mode" = 'thin' ]; then
-	date +\ %H:%M:%S\ %a%b\ %e | wrap-to 3
+	#date +\ %H:%M:%S\ %a%b\ %e | wrap-to 3
+	date +\ %H%n:%M%n:%S%n%a%n%b%n\ %e
 else
 	date +\ %H:%M:%S%n%A,\ %B\ %e
 fi
