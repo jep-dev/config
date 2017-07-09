7b79597b2d764ac6bb5baabfea90d103e8a76a1d
John Petersen <jepetersen.dev@gmail.com>
2017-07-09 16:52:40 -0400

    Added troubleshooting; some duplicate instances of the Triggerhappy daemon were found but this may not be an issue with my script

--- bin/thd.sh
+++ bin/thd.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
-# echo "User: $USER"
-
-# --triggers /home/john/.triggerhappy.conf \
-thd --daemon --user john \
-	--socket /var/run/thd.socket \
-	--pidfile /var/run/thd.pid \
-	--triggers /home/john/triggerhappy.conf \
-	/dev/input/event*
-# --user nobody
+pidof thd >/dev/null || \
+	thd --daemon --user john \
+		--socket /var/run/thd.socket --pidfile /var/run/thd.pid \
+		--triggers /home/john/triggerhappy.conf /dev/input/event* \
+		&& { pidof thd >/dev/null || echo 'Test contains false negatives' }
