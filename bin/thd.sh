bd64c43a6eafa0ea513faf0599eef50563706730
John Petersen <jepetersen.dev@gmail.com>
2017-10-29 16:46:45 -0400

    Removed a few scripts that fell out of use

--- bin/thd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-pidof thd >/dev/null || \
-	thd --daemon --user john \
-		--socket /var/run/thd.socket --pidfile /var/run/thd.pid \
-		--triggers /home/john/triggerhappy.conf /dev/input/event* \
-		&& { pidof thd >/dev/null || echo 'Test contains false negatives' }
